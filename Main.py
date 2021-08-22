9392d117326cc3bae13f897426bb50b56a4405dc
Ben <btaylor280@gmail.com>
2021-08-22 16:53:24 -0500

    Adding more! :memo:

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import tweepy
 import News
 from decouple import config
+from random import randint
+from time import sleep
+
 
 
 # Authenticate to Twitter
@@ -13,6 +16,8 @@ auth.set_access_token(config('key'),
 api = tweepy.API(auth)
 # import news
 title, url = News.NewsFromAPI()
+# Adding random sleep function to tweet
+sleep(randint(100,36000))
 # Create a tweet
 api.update_status(title + " " + url)
 
--- News.py
+++ News.py
@@ -3,7 +3,7 @@ import requests
 from decouple import config
 def NewsFromAPI():
 	
-	# BBC news api
+	# News api
 	# following query parameters are used
 	# source, sortBy and apiKey
 	query_params = {
@@ -11,34 +11,20 @@ def NewsFromAPI():
 	"sortBy": "top",
 	"apiKey": config('apiKey'),
     "q": "security",
-    "q": "Hacker"
+    "q": "computer"
 	}
 	main_url = " https://newsapi.org/v2/top-headlines"
 
 	# fetching data in json format
 	res = requests.get(main_url, params=query_params)
-	open_bbc_page = res.json()
+	open_page = res.json()
 
 	# getting all articles in a string article
-	article = open_bbc_page["articles"]
+	article = open_page["articles"]
 
-	# empty list which will
-	# contain all trending news
-	  #print(article[0]["title"])
-    
 	title = (article[0]["title"])
 	url = (article[0]["url"])
-	#print (url)
-	#print (title)
-	#results = []
-	#for ar in article:
-	#	#results.append(ar["source"])
-	#	print (ar["url"])
-	#	
-	#for i in range(len(results)):
-	#	
-	#	# printing all trending news
-	#	print(i + 1, results[i])
+
 	return title,url
 
 # function call
